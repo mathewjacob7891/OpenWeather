0d1712c7e292820f94f63fb415a253e475f0baee
Mathew Jacob <mathew.jacob@ftr.com>
2023-03-13 04:04:05 +0530

    Open Weather Project - PSA
    Changes :
    - Handled navigation correctly.
    
    TODO :
    - Need to add Unit Test Cases.
    - Need to move some more elements to data binding.

--- app/src/main/java/com/mathew/openweather/view/AddCityFragment.kt
+++ app/src/main/java/com/mathew/openweather/view/AddCityFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.mathew.openweather.databinding.FragmentAddCityBinding
 import com.mathew.openweather.viewmodel.AddCityViewModel
 
@@ -29,6 +30,15 @@ class AddCityFragment : Fragment() {
         _binding = FragmentAddCityBinding.inflate(inflater, container, false)
         binding.viewModel = addCityViewModel
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        addCityViewModel.navigationLiveData.observe(viewLifecycleOwner) {
+            if (it) {
+                findNavController().popBackStack()
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/mathew/openweather/viewmodel/AddCityViewModel.kt
+++ app/src/main/java/com/mathew/openweather/viewmodel/AddCityViewModel.kt
@@ -3,6 +3,8 @@ package com.mathew.openweather.viewmodel
 import android.app.Application
 import androidx.databinding.ObservableField
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.mathew.openweather.repository.CityRepository
 import kotlinx.coroutines.Dispatchers
@@ -12,6 +14,8 @@ class AddCityViewModel(application: Application) : AndroidViewModel(application)
 
     private var repository: CityRepository
     val cityName = ObservableField<String>()
+    private var _navigationLiveData = MutableLiveData(false)
+    var navigationLiveData: LiveData<Boolean> = _navigationLiveData
 
     init {
         repository = CityRepository(application)
@@ -21,6 +25,7 @@ class AddCityViewModel(application: Application) : AndroidViewModel(application)
         cityName.get()?.let {
             viewModelScope.launch(Dispatchers.IO) {
                 repository.addCity(it)
+                _navigationLiveData.postValue(true)
             }
         }
     }
